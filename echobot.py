33c961aab263e8a49f3c1129c70223e563b1362d
Thomas Slaymaker <thomas.slaymaker@gmail.com>
2018-09-03 13:11:17 -0600

    initial commit, work in progress

---
+++ echobot.py
@@ -0,0 +1,78 @@
+import json
+import requests
+import time
+import urllib
+
+TOKEN = "642835760:AAFn5bXQ5Iy5jP3p5UiMPQbrZFEv5pghHL8"
+URL = "https://api.telegram.org/bot{}/".format(TOKEN)
+
+
+def get_url(url):
+    response = requests.get(url)
+    content = response.content.decode("utf8")
+    return content
+
+
+def get_json_from_url(url):
+    content = get_url(url)
+    js = json.loads(content)
+    return js
+
+
+def get_updates(offset=None):
+    """
+    Used to poll the JSON file for a given telegram bot
+    :param offset: Used to identify the most recent updates and account for which ones have been acknowledged
+    :return: returns the json of the url call
+    """
+    # timeout is used to enable "long polling" essentially reducing resource load
+    url = URL + "getUpdates?timeout=100"
+    if offset:
+        url += "&offset={}".format(offset)
+    js = get_json_from_url(url)
+    return js
+
+
+def get_last_chat_id_and_text(updates):
+    num_updates = len(updates["result"])
+    last_update = num_updates - 1
+    text = updates["result"][last_update]["message"]["text"]
+    chat_id = updates["result"][last_update]["message"]["chat"]["id"]
+    return text, chat_id
+
+
+def get_last_update_id(updates):
+    update_ids = []
+    for update in updates["result"]:
+        update_ids.append(int(update["update_id"]))
+    return max(update_ids)
+
+
+def send_message(text, chat_id):
+    text = urllib.parse.quote_plus(text)
+    url = URL + "sendMessage?text={}&chat_id={}".format(text, chat_id)
+    get_url(url)
+
+
+def echo_all(updates):
+    for update in updates["result"]:
+        try:
+            text = update["message"]["text"]
+            chat = update["message"]["chat"]["id"]
+            send_message(text, chat)
+        except Exception as e:
+            print(e)
+
+
+def main():
+    last_update_id = None
+    while True:
+        updates = get_updates(last_update_id)
+        if len(updates["result"]) > 0:
+            last_update_id = get_last_update_id(updates) + 1
+            echo_all(updates)
+        time.sleep(0.5)
+
+
+if __name__ == '__main__':
+    main()
