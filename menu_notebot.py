7ac61d61ea44da8a2e9037f9843bca0c531aebb7
Thomas Slaymaker <tslaymaker@progrexion.com>
2018-09-11 18:05:26 -0600

    started notebot with menus enchancement

---
+++ menu_notebot.py
@@ -0,0 +1,135 @@
+#!/usr/bin/python3
+from telegram.ext import Updater
+from telegram.ext import CommandHandler, CallbackQueryHandler
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup
+import yaml
+
+
+# Bot
+def start(bot, update):
+    update.message.reply_text(main_menu_message(), reply_markup=main_menu_keyboard())
+
+
+def main_menu(bot, update):
+    query = update.callback_query
+    bot.edit_message_text(chat_id=query.message.chat_id,
+                          message_id=query.message.message_id,
+                          text=main_menu_message(),
+                          reply_markup=main_menu_keyboard())
+
+
+def take_note_menu(bot, update):
+    query = update.callback_query
+    bot.edit_message_text(chat_id=query.message.chat_id,
+                          message_id=query.message.message_id,
+                          text=take_note_menu_message(),
+                          reply_markup=take_note_menu_keyboard())
+
+
+def review_notes_menu(bot, update):
+    query = update.callback_query
+    bot.edit_message_text(chat_id=query.message.chat_id,
+                          message_id=query.message.message_id,
+                          text=review_notes_menu_message(),
+                          reply_markup=review_notes_menu_keyboard())
+
+
+def search_notes_menu(bot, update):
+    query = update.callback_query
+    bot.edit_message_text(chat_id=query.message.chat_id,
+                          message_id=query.message.message_id,
+                          text=search_notes_menu_message(),
+                          reply_markup=search_notes_menu_keyboard())
+
+
+# sub menus and actions
+def take_note_submenu(bot, update):
+    pass
+
+
+def review_note_submenu(bot, update):
+    pass
+
+
+def search_note_submenu(bot, update):
+    pass
+
+
+# Keyboards
+def main_menu_keyboard():
+    keyboard = [[InlineKeyboardButton('Take a new note', callback_data='m1')],
+                [InlineKeyboardButton('Review my notes', callback_data='m2')],
+                [InlineKeyboardButton('Search all notes', callback_data='m3')]]
+    return InlineKeyboardMarkup(keyboard)
+
+
+def take_note_menu_keyboard():
+    note_categories = set_note_categories()
+    m_count = 1
+    keyboard = []
+    for note in note_categories:
+        keyboard.append([InlineKeyboardButton(note, callback_data='m1_{}'.format(m_count))])
+        m_count += 1
+    keyboard.append([InlineKeyboardButton('Main menu', callback_data='main')])
+    return InlineKeyboardMarkup(keyboard)
+
+
+def review_notes_menu_keyboard():
+    keyboard = [[InlineKeyboardButton('My notes', callback_data='m2_1')],
+                [InlineKeyboardButton('Edit previous note', callback_data='m2_2')],
+                [InlineKeyboardButton('Main menu', callback_data='main')]]
+    return InlineKeyboardMarkup(keyboard)
+
+
+def search_notes_menu_keyboard():
+    keyboard = [[InlineKeyboardButton('By category', callback_data='m3_1')],
+                [InlineKeyboardButton('By note taker', callback_data='m3_2')],
+                [InlineKeyboardButton('Last x number of notes', callback_data='m3_3')],
+                [InlineKeyboardButton('Main menu', callback_data='main')]]
+    return InlineKeyboardMarkup(keyboard)
+
+
+def enter_description_keyboard():
+    pass
+
+
+# Messages
+def main_menu_message():
+    return 'What would you like dad_bot to do?'
+
+
+def take_note_menu_message():
+    return 'Choose a note category:'
+
+
+def review_notes_menu_message():
+    return 'What do you need to do with your notes?'
+
+
+def search_notes_menu_message():
+    return 'How would you like to search your notes?'
+
+
+# Helper Functions
+def set_note_categories():
+    with open('note_categories.txt', 'r') as note_file:
+        notes = note_file.read().splitlines()
+    return notes
+
+
+# Handlers
+
+with open('credentials.yml', 'r') as infile:
+    creds = yaml.load(infile)
+updater = Updater("{}".format(creds['api_key']))
+
+updater.dispatcher.add_handler(CommandHandler('start', start))
+updater.dispatcher.add_handler(CallbackQueryHandler(main_menu, pattern='main'))
+updater.dispatcher.add_handler(CallbackQueryHandler(take_note_menu, pattern='m1'))
+updater.dispatcher.add_handler(CallbackQueryHandler(review_notes_menu, pattern='m2'))
+updater.dispatcher.add_handler(CallbackQueryHandler(search_notes_menu, pattern='m3'))
+updater.dispatcher.add_handler(CallbackQueryHandler(take_note_submenu, pattern='m1_1'))
+updater.dispatcher.add_handler(CallbackQueryHandler(review_note_submenu, pattern='m2_1'))
+updater.dispatcher.add_handler(CallbackQueryHandler(search_note_submenu, pattern='m3_1'))
+
+updater.start_polling()
