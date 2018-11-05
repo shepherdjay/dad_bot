17e88899fdc07916e921abf54bbe6c025b3a41db
Jay Shepherd <shepherdjay@users.noreply.github.com>
2018-11-05 14:35:00 -0700

    Change add_item and category query

--- dbhelper.py
+++ dbhelper.py
@@ -1,10 +1,10 @@
 from sqlalchemy import Column, String, Integer, DateTime, Index
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
 
-class SqlAlchemyMigration(object):
+class SqlAlch(object):
     Base = declarative_base()
 
     class Items(Base):
@@ -25,7 +25,7 @@ class DBHelper:
     def __init__(self, dbname="dadinfo.sqlite"):
         self.dbname = dbname
         self.alchemyengine = create_engine("sqlite:///" + dbname)
-        self.alchemybase = SqlAlchemyMigration.Base
+        self.alchemybase = SqlAlch.Base
         self.conn = self.alchemyengine.raw_connection()
         self._session = sessionmaker(bind=self.alchemyengine)
         self.session = self._session()
@@ -36,10 +36,10 @@ class DBHelper:
         self.alchemybase.metadata.create_all(self.alchemyengine)
 
     def add_item(self, item_text, owner_id, category, owner_name, datetime):
-        stmt = "INSERT INTO items (description, owner_id, category, owner_name, datetime) VALUES (?, ?, ?, ?, ?)"
-        args = (item_text, owner_id, category, owner_name, datetime)
-        self.conn.execute(stmt, args)
-        self.conn.commit()
+        self.session.add(
+            SqlAlch.Items(description=item_text, owner_id=owner_id, category=category, owner_name=owner_name,
+                          datetime=datetime))
+        self.session.commit()
 
     def delete_item(self, item_text, owner_id):
         stmt = "DELETE FROM items WHERE description = (?) AND owner_id = (?)"
@@ -62,9 +62,10 @@ class DBHelper:
         stmt = "SELECT description FROM items WHERE category = (?) ORDER BY datetime LIMIT = (?)"
         args = (category, limit,)
         if datetime:
-            stmt = "SELECT description, datetime FROM items WHERE category = (?) ORDER BY datetime DESC LIMIT (?)"
-            args = (category, limit,)
-            return self.conn.execute(stmt, args)
+            return self.session.query(
+                SqlAlch.Items.description, SqlAlch.Items.datetime).filter(
+                SqlAlch.Items.category == category).order_by(
+                SqlAlch.Items.datetime.desc())[:limit]
         return [x[0] for x in self.conn.execute(stmt, args)]
 
     def get_last_x_requested_items(self, number_of_events, datetime=False):
--- tests/test_dbhelper.py
+++ tests/test_dbhelper.py
@@ -17,10 +17,16 @@ def testdb():
 def populated_testdb(testdb):
     item1 = {"item_text": "This is a test item", "owner_id": "1", "category": "Medication",
              "owner_name": "Nobody"}
-    item2 = {"item_text": "This is a test item", "owner_id": "2", "category": "Medication",
+    item2 = {"item_text": "This is a second test item", "owner_id": "2", "category": "Medication",
+             "owner_name": "Somebody"}
+    item3 = {"item_text": "This is a special test item", "owner_id": "1", "category": "Special",
+             "owner_name": "Somebody"}
+    item4 = {"item_text": "This is a second special test item", "owner_id": "2", "category": "Special",
              "owner_name": "Somebody"}
     testdb.add_item(**item1, datetime=datetime.datetime.now())
     testdb.add_item(**item2, datetime=datetime.datetime.now())
+    testdb.add_item(**item3, datetime=datetime.datetime.now())
+    testdb.add_item(**item4, datetime=datetime.datetime.now())
     return testdb
 
 
@@ -28,7 +34,7 @@ def test_add_item_and_basic_retrieve(testdb):
     #     def add_item(self, item_text, owner_id, category, owner_name, datetime):
     testdb.add_item(item_text="This is a test item", owner_id="123456789", category="Medication", owner_name="Nobody",
                     datetime=datetime.datetime.now())
-    testdb.add_item(item_text="This is a test item 2", owner_id="666", category="Medication", owner_name="Somebody",
+    testdb.add_item(item_text="This is a second test item", owner_id="666", category="Medication", owner_name="Somebody",
                     datetime=datetime.datetime.now())
     item_count = testdb.conn.execute("SELECT COUNT(*) from items").fetchone()[0]
     assert ["This is a test item"] == testdb.get_items_by_owner_id(owner_id=123456789)
@@ -36,8 +42,12 @@ def test_add_item_and_basic_retrieve(testdb):
 
 
 def test_get_items_by_category_with_datetime(populated_testdb):
-    assert 2 == len(list(populated_testdb.get_items_by_category("Medication", limit=5, datetime=True)))
-
+    category_items = [_[0] for _ in list(populated_testdb.get_items_by_category("Medication", limit=5, datetime=True))]
+    assert 2 == len(category_items)
+    assert "This is a test item" in category_items
+    assert "This is a second test item" in category_items
 
 def test_get_items_by_owner_id(populated_testdb):
-    assert 1 == len(list(populated_testdb.get_items_by_owner_id(1)))
+    owner_items = list(populated_testdb.get_items_by_owner_id(1))
+    assert 2 == len(owner_items)
+    assert "This is a test item" in owner_items
